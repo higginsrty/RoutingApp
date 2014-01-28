b407c84e6d1b79c23f39589dc608db2e362b52d3
whiteSIU <riley_is_white@hotmail.com>
2014-01-28 12:04:26 -0600

    flooding files

---
+++ NetworkSim/floodingRT.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+
+FloodingRT::FloodingRT(QQuickItem *parent) :
+    QQuickItem(parent)
+{
+}
+
+void Flood::floodNetwork(Node *node1, Packet *packet1)
+{
+    while(sourceNodeAcknowlPacket =! true)
+    {
+
+
+
+
+    }
+}
+
+
+
+
+
---
+++ NetworkSim/floodingRT.h
@@ -0,0 +1,14 @@
+#ifndef FLOODINGRT_H
+#define FLOODINGRT_H
+
+#include "stdafx.h"
+
+
+
+class FloodingRT : public QQuickItem
+{
+
+
+
+};
+#endif // FLOODINGRT_H
