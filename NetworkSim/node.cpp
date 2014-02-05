d5ed9fc55efa634bc1c09ee27eaf96581c94dfe5
whiteSIU <riley_is_white@hotmail.com>
2014-02-05 11:13:17 -0600

    node files updated (not complete)

--- NetworkSim/node.cpp
+++ NetworkSim/node.cpp
@@ -48,3 +48,29 @@ QObject* Node::get_q_object() {
 int Node::get_id(){
     return node_id;
 }
+
+//added 2/3/14
+int Node::process_packet(Packet packet){
+    int destination_node = packet.get_destination_node()->get_id();
+
+    if(destination_node != node_id)
+    {
+        if(routing_table.get_cost(destination_node) != -1)
+            return routing_table.get_cost(destination_node) + routing_table.get_next_hop(destination_node)->process_packet(packet);
+        else
+            update_packets.push_back(packet);
+    }
+    else
+    {   //I'm assuming we're going to add data packets??
+        if(packet.get_packet_type() == Packet::data)
+            return 0;
+        else
+        {
+            if(process_packet(packet))
+                send_update_packets(packet.get_time());
+            return 0;
+        }
+    }
+}//end of process_packet
+
+
--- NetworkSim/node.h
+++ NetworkSim/node.h
@@ -4,6 +4,16 @@
 #include "stdafx.h"
 
 class Link;
+class Packet;
+
+struct Neighbor{
+    int neighbor_ID;
+    double link_delay;
+    Neighbor(){neighbor_ID = 0; link_delay = 0;}
+    Neighbor(int ID, double delay){neighbor_ID = ID; link_delay = delay;}
+};
+
+
 
 class Node : public QQuickItem
 {
@@ -20,11 +30,31 @@ public:
     QObject* get_q_object();
     void set_id(int id);
     int node_weight = 0;
+
+    //added 2/4/14
+    std::vector<int> get_neighbor_vector();
+
+    void add_route(Node* destination_node, Node* next_hop, int weight);
+    void remove_link(int linked_node_id, double weight);
+    int get_link_cost(int linked_nod_id);
+    /*
+    std::vector<dv> get_dvr_table(int destination_node);
+
+    std::vector<Packet> get_update_packets();
+    */
+
+    int process_packet(Packet packet);
+
+
+
 private:
     int node_id;
     QString name;
     bool source;
     QObject* node;
+
+    bool process_dv_packet(Packet packet);
+    void send_update_packets(double weight);
 
 signals:
 
